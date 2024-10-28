3c0bfdc6f18d36f2cef43a01557c62cc5ae3ea86
Yazid Bintang <yazidbintang2711@gmail.com>
2024-10-28 19:11:47 +0700

    Add require dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
    alias(libs.plugins.android.application)
    alias(libs.plugins.kotlin.android)
+   id("com.google.devtools.ksp")
+   id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -33,6 +35,9 @@ android {
    kotlinOptions {
       jvmTarget = "1.8"
    }
+   buildFeatures {
+      viewBinding = true
+   }
 }
 
 dependencies {
@@ -45,4 +50,34 @@ dependencies {
    testImplementation(libs.junit)
    androidTestImplementation(libs.androidx.junit)
    androidTestImplementation(libs.androidx.espresso.core)
+   implementation("androidx.activity:activity-ktx:1.9.3")
+
+   // Dagger Hilt
+   implementation("com.google.dagger:hilt-android:2.49")
+   ksp("com.google.dagger:dagger-compiler:2.49")
+   ksp("com.google.dagger:hilt-compiler:2.49")
+
+   //SplashScreen
+   implementation ("androidx.core:core-splashscreen:1.0.1")
+
+   // Retrofit
+   implementation("com.squareup.retrofit2:retrofit:2.9.0")
+   implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+   // OkHttp and Chucker
+   implementation("com.squareup.okhttp3:okhttp:4.12.0")
+   debugImplementation("com.github.chuckerteam.chucker:library:4.0.0")
+   releaseImplementation("com.github.chuckerteam.chucker:library-no-op:4.0.0")
+
+   // Coroutines
+   implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+   // Lifecycle
+   implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+   implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
+   // Glide
+   implementation("com.github.bumptech.glide:glide:4.16.0")
+   annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+   dependencies {
+      classpath("com.google.dagger:hilt-android-gradle-plugin:2.49")
+   }
+}
+
 plugins {
    alias(libs.plugins.android.application) apply false
    alias(libs.plugins.kotlin.android) apply false
+   id("com.google.devtools.ksp") version "1.9.20-1.0.13" apply false
 }
